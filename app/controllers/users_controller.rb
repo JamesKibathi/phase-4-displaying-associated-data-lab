ad9db6b3f099d91ff3d6b7e11ec8f1d7b27afc2b
James Njenga <knjenga94@gmail.com>
2022-11-27 20:51:37 +0300

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+     # resources :items, only: [:index]
+    # resources :users, only: [:show]
+
+    def show
+        user=User.find_by(id:params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "User not found" }, status: :not_found
+    end
+  
 end
